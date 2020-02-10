0dffd4bdb87b110b02ddf9accc06945709ec234a
Sarah <sarah.bradford1@marist.edu>
2020-02-09 23:08:39 -0500

    made some edits, added citations, still needs work

---
+++ game.py
@@ -0,0 +1,80 @@
+# name: Sarah Bradford
+# date: 2/10/2020
+
+
+def game():
+    enter="press enter to continue"
+    score=0
+    name=input("enter your name")
+    favoriteplayer=input("enter your favorite Red Sox player")
+    
+
+    print(name, ", I am running late. Help me get to the Red Sox game tonight!")
+
+    print() # Prints a blank line
+    print("My dad just gave me his tickets to the baseball game tonight, but I am running late and I have to pick up my friend and then take the T into Boston.")
+    print("I can't wait to see", favoriteplayer,)
+    print("Score:", score)
+    input(enter)
+    print()
+
+    print("Oh no", name, "! My car is almost out of gas. I have to stop and fill up your tank.")
+    print("Score:", score + 50)
+    input(enter)
+    print()
+
+    print("I get to my friends house, but she isn't ready to go yet. 10 minutes later she finally gets into the car and we head on our way.")
+    print("Score:", score + 100)
+    input(enter)
+    print()
+
+    print("I get to the T station but, it already left without us.")
+    print("We have to drive to another station to get to another T.")
+    print("Score:", score + 150)
+    input(enter)
+    print()
+
+    print("We finally get on the T and head into Boston.")
+    print("Score:", score + 200)
+    input(enter)
+    print()
+
+    print("We have arrived in Boston!")
+    print("Score:", score + 250)
+    input(enter)
+    print()
+
+    print("Now we Uber to Fenway, and everything seems to be going our way.")
+    print("Score:", score + 300)
+    input(enter)
+    print()
+
+    print("We finally get to Fenway and can't find our tickets in my backpack.")
+    print("Yes!! I finally found them in my pocket.")
+    print("Score:", score + 350)
+    input(enter)
+    print()
+
+    print("The game is about to start, we cut it close.")
+    print("Score:", score + 400)
+    input(enter)
+    print()
+
+    print("My favorite player" , favoriteplayer, "hit a homerun right in the vacinity of my seat!")
+    print("I caught the ball!!")
+    print("Score:", score + 450)
+    input(enter)
+    print()
+    
+    print("The Red Sox won the game! They are now doing post game interviews with some of our favorite players!")
+    print("Score:", score + 500)
+    input(enter)
+    print()
+
+    print(name, "Congratulations, you have won! Thank you for your help! Would you like to play again?")
+
+def main():
+    game()
+    print ("citations: got template from iLearn, collaborated with Jadyn Kennedy and Michael Conger")
+
+main()
