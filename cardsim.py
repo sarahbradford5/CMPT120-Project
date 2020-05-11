e1768b1b5744605c98daf7ed84c1c2a0637e8c69
sarah <sarah.bradford1@marist.edu>
2020-05-10 23:09:51 -0400

    finalized card simulator

--- cardsim.py
+++ cardsim.py
@@ -1,14 +1,11 @@
 #Sarah Bradford
-#Project 5
+#Project 5 - card simulator
+# 2020-05-11
 
-#52 cards in a deck 
-    #for each suit there must be 13 cards with distinct rank
 
-#getSuit -> returns what the suit card is
-#getRank -> returns waht the rank card it 
+print("Card Simulator")
 
-print("Card Simulator Game")
-
+import random 
 class Card:
     def __init__ (self, suit, rank):
         self.suit = suit 
@@ -19,7 +16,6 @@ class Card:
 
     def getRank(self):
         return self.rank
-
 
 class Deck:
     def __init__ (self): #need 52 cards in deck in this instance variable
@@ -48,13 +44,24 @@ class Deck:
                     card = Card(suit, "king")
                 deck.append(card)
         self.deck = deck
+    def shuffle(self):
+        random.shuffle(self.deck)
+        for z in range(len(self.deck) -1, 0, -1):
+            s = random.randint(0, z)
+            self.deck[z], self.deck[s] = self.deck[s], self.deck[z]
+    def draw(self):
+        card = self.deck.pop(0)
+        x = card.getSuit() + " " + card.getRank()
+        return x 
+        
 
-    
-def main():
+def main():  
     cards = Deck()
-    deck = cards.deck
-    print(len(deck))
-    for c in deck:
-        print(c.getSuit() + " " + c.getRank())
-    
+    cards.shuffle()
+    for x in range(0,52):
+        print(cards.draw())
+
 main()
+
+
+
